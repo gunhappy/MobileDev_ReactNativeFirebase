f3b122f51424d3cbee14a8ddd77bc6e9ec670a12
gunhappy <phasin.s@ku.th>
2018-04-19 12:56:17 +0700

    fetch data from randomuser and firebase user

--- consts/firebase_info.js
+++ consts/firebase_info.js
@@ -1,31 +1,8 @@
 export const config = {
-  apiKey: "AIzaSyAzgTI4wEpt6FxJAR2tX45WhdQzNT8MbF4",
-  authDomain: "myapp-f38c1.firebaseapp.com",
-  databaseURL: "https://myapp-f38c1.firebaseio.com",
-  projectId: "myapp-f38c1",
-  storageBucket: "",
-  messagingSenderId: "357443056489"
+  apiKey: "AIzaSyAjXQWUa305_NamtBuJRd8czSBolTfV5Es",
+  authDomain: "react-native-firebase-login.firebaseapp.com",
+  databaseURL: "https://react-native-firebase-login.firebaseio.com",
+  projectId: "react-native-firebase-login",
+  storageBucket: "react-native-firebase-login.appspot.com",
+  messagingSenderId: "241579730001"
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export const config = {
-//   apiKey: "AIzaSyDaY9hbGQOr41ljQeZgX26NTd_SqKwH4oQ",
-//   authDomain: "studentloan-7fbce.firebaseapp.com",
-//   databaseURL: "https://studentloan-7fbce.firebaseio.com",
-//   projectId: "studentloan-7fbce",
-//   storageBucket: "studentloan-7fbce.appspot.com",
-//   messagingSenderId: "1016135551882"
-// };
--- screens/BalanceScreen.js
+++ screens/BalanceScreen.js
@@ -1,11 +1,63 @@
-import React, { Component } from 'react';
-import { Text} from 'react-native';
+import React, { Component } from 'react'
+import { Text, View, FlatList} from 'react-native'
+import { List, ListItem } from "react-native-elements"
+import firebase from 'firebase'
 
 class BalanceScreen extends Component {
+    static navigationOptions = {
+        title: 'Users',
+        header: null
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+          loading: false,
+          data: [],
+          error: null,
+          refreshing: false
+        }
+    }
+
+    componentDidMount() {
+        this.getUsersFromFirebase()
+    }
+
+    getUsersFromFirebase() {
+        var self = this
+        const userRef = firebase.database().ref('/users')
+        userRef.orderByKey().once('value')
+        .then(function(data) {
+            data.forEach(element => {
+                const dataArray = self.state.data
+                dataArray.push(element.val().userDetails)
+                self.setState({
+                    data: dataArray
+                })
+            });
+        })
+    }
+
+      
     render() {
+        if(this.state.data.length === 0) return <View/>
         return (
-          <Text>Another Screen</Text>  
-        );
+            <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
+                <FlatList 
+                data = {this.state.data}
+                renderItem = {
+                    ({ item }) => (
+                    <ListItem
+                        title={`${item.firstname} ${item.lastname}`}
+                        subtitle={item.email}
+                        containerStyle={{ borderBottomWidth: 0 }}
+                    />
+                    )
+                }
+                keyExtractor={item => item.email}
+                />
+            </List> 
+        )
     }
 }
 export default BalanceScreen;
